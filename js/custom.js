8e36115ef83178ff699470f46a50999d491b0bc1
Moustafa <moustafafci25@gmail.com>
2020-02-01 11:16:39 +0200

    add contact us and footer sections

--- js/custom.js
+++ js/custom.js
@@ -3,25 +3,16 @@ $(function () {
 
     // trigger counter up plugin 
     $('[data-toggle = counterup]').counterUp({
-        delay : 10 ,
-        time : 1000
+        delay: 10,
+        time: 1000
     });
 
+    $('#portfolio ul li').on('click', function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    });
     // trigger mixitup plugin
     var mixer = mixitup('.portfolio');
 
-    // trigger owlcarousel
- //   $('.owl-carousel').owlCarousel();
-    $('.owl-carousel').owlCarousel({
-        animateOut: 'slideOutDown',
-        animateIn: 'flipInX',
-        items:1,
-        loop:true,
-        margin:30,
-        stagePadding:30,
-        smartSpeed:450
-    });
-
-        // trigger wow plugin
-        new WOW().init();
+    // trigger wow plugin
+    new WOW().init();
 });
